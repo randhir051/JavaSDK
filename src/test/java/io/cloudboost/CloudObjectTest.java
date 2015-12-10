2ef80e3240971b4824d2e5a50183dbf48ffb610a
nawazdhandala <egimaben@gmail.com>
2015-12-10 23:36:03 +0300

    bug fix

--- src/test/java/io/cloudboost/CloudObjectTest.java
+++ src/test/java/io/cloudboost/CloudObjectTest.java
@@ -292,7 +292,7 @@ public class CloudObjectTest{
 	public void saveArrayWithJSONObject() throws CloudException{
 		initialize();
 		CloudObject obj = new CloudObject("DATA_1");
-		obj.set("name", "sample");
+		obj.set("name", "ayiko");
 		JSONObject obj1 = new JSONObject();
 		try {
 			obj1.put("sample", "sample");
